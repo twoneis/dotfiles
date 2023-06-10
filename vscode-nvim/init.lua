3496a35d5cc439051007bebf86017c04d57e3d0b
twoneis <sanjay29@sapura.de>
2023-06-10 12:10:07 +0200

    seperate vs code neovim config

---
+++ vscode-nvim/init.lua
@@ -0,0 +1,6 @@
+-- remap navigation for dvorak and personal preferences
+vim.keymap.set({"n", "v", "o"}, "t", "k")
+vim.keymap.set({"n", "v", "o"}, "n", "j")
+vim.keymap.set({"n", "v", "o"}, "s", "l")
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
